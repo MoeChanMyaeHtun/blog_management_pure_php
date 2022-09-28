8df1b705aeaf9098d5caa1060f56ec1851de5bba
MoeChanMyaeTun <scm.moechanmyaehtun@gmail.com>
2022-09-28 16:52:21 +0630

    common

--- common/header.php
+++ common/header.php
@@ -11,6 +11,7 @@
   <link rel="stylesheet" href="../css/login.css">
   <link rel="stylesheet" href="../css/categories.css">
   <link rel="stylesheet" href="../css/nav.css">
+  <link rel="stylesheet" href="../css/post.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
